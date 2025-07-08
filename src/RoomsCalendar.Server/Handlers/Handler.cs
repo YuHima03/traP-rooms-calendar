4df1e5da49f44824444d9ca5f1b339d6c4e24ada
YuHima03 <yuhima03@gmail.com>
2025-07-08 20:03:34 +0900

    ✨ MinimalAPI に移行

---
+++ src/RoomsCalendar.Server/Handlers/Handler.cs
@@ -0,0 +1,13 @@
+﻿namespace RoomsCalendar.Server.Handlers
+{
+    public class Handler
+    {
+        readonly RoomsHandler _roomsHandler = new();
+
+        public void MapHandlers(IEndpointRouteBuilder builder)
+        {
+            var apiGroup = builder.MapGroup("api");
+            _roomsHandler.MapHandlers(apiGroup);
+        }
+    }
+}
