38ca4f279c5ab17e135bf4ec9a31242c93abad4e
YuHima03 <yuhima03@gmail.com>
2025-07-08 20:08:37 +0900

    ✨ /api/events を生やした

--- src/RoomsCalendar.Server/Handlers/Handler.cs
+++ src/RoomsCalendar.Server/Handlers/Handler.cs
@@ -2,11 +2,13 @@
 {
     public class Handler
     {
+        readonly EventsHandler _eventsHandler = new();
         readonly RoomsHandler _roomsHandler = new();
 
         public void MapHandlers(IEndpointRouteBuilder builder)
         {
             var apiGroup = builder.MapGroup("api");
+            _eventsHandler.MapHandlers(apiGroup);
             _roomsHandler.MapHandlers(apiGroup);
         }
     }
