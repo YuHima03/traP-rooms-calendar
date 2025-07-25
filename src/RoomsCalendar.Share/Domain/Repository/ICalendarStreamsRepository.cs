927e61cb831116a8d6d49d7d5f37ab914733604a
YuHima03 <yuhima03@gmail.com>
2025-07-25 17:18:48 +0900

    ✨ IRepositoryBase 追加

--- src/RoomsCalendar.Share/Domain/Repository/ICalendarStreamsRepository.cs
+++ src/RoomsCalendar.Share/Domain/Repository/ICalendarStreamsRepository.cs
@@ -1,6 +1,6 @@
 ﻿namespace RoomsCalendar.Share.Domain.Repository
 {
-    public interface ICalendarStreamsRepository
+    public interface ICalendarStreamsRepository : IRepositoryBase
     {
         public ValueTask<CalendarStream?> TryGetCalendarStreamAsync(Guid streamId, CancellationToken ct = default);
 
